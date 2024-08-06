c977f63c061e752da85329e1639b75053aa55b1a
Pedro Barreto <pedro.barreto.ne@gmail.com>
2024-08-06 10:50:34 -0300

    att:registerForm

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,24 +1,29 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
 interface SignupForm {
-  name: FormControl,
-  email: FormControl,
-  cep: FormControl,
-  street: FormControl,
-  city: FormControl,
-  state: FormControl,
-  country: FormControl,
-  cpfCnpj: FormControl,
-  birthDate: FormControl,
-  phoneNumber: FormControl,
-  password: FormControl,
-  passwordConfirm: FormControl
+  name: FormControl;
+  email: FormControl;
+  cep: FormControl;
+  street: FormControl;
+  city: FormControl;
+  state: FormControl;
+  country: FormControl;
+  cpfCnpj: FormControl;
+  birthDate: FormControl;
+  phoneNumber: FormControl;
+  password: FormControl;
+  passwordConfirm: FormControl;
 }
 
 @Component({
@@ -27,13 +32,11 @@ interface SignupForm {
   imports: [
     DefaultLoginLayoutComponent,
     ReactiveFormsModule,
-    PrimaryInputComponent
+    PrimaryInputComponent,
   ],
-  providers: [
-    LoginService
-  ],
+  providers: [LoginService],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignUpComponent {
   signupForm!: FormGroup<SignupForm>;
@@ -42,7 +45,7 @@ export class SignUpComponent {
     private router: Router,
     private loginService: LoginService,
     private toastService: ToastrService
-  ){
+  ) {
     this.signupForm = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -51,22 +54,51 @@ export class SignUpComponent {
       city: new FormControl('', [Validators.required]),
       state: new FormControl('', [Validators.required]),
       country: new FormControl('', [Validators.required]),
-      cpfCnpj: new FormControl('', [Validators.required, Validators.minLength(11)]),
+      cpfCnpj: new FormControl('', [
+        Validators.required,
+        Validators.minLength(11),
+      ]),
       birthDate: new FormControl('', [Validators.required]),
-      phoneNumber: new FormControl('', [Validators.required, Validators.minLength(11)]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    })
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.minLength(11),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      passwordConfirm: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
 
-  submit(){
-    this.loginService.signup(this.signupForm.value.name, this.signupForm.value.email, this.signupForm.value.cep, this.signupForm.value.cpfCnpj, this.signupForm.value.birthDate, this.signupForm.value.phoneNumber, this.signupForm.value.password).subscribe({
-      next: () => this.toastService.success("Registro feito com sucesso!"),
-      error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde")
-    })
+  submit() {
+    this.loginService
+      .signup(
+        this.signupForm.value.name,
+        this.signupForm.value.email,
+        this.signupForm.value.cep,
+        this.signupForm.value.street,
+        this.signupForm.value.city,
+        this.signupForm.value.state,
+        this.signupForm.value.country,
+        this.signupForm.value.cpfCnpj,
+        this.signupForm.value.birthDate,
+        this.signupForm.value.phoneNumber,
+        this.signupForm.value.password
+      )
+      .subscribe({
+        next: () => this.toastService.success('Registro feito com sucesso!'),
+        error: () =>
+          this.toastService.error(
+            'Erro inesperado! Tente novamente mais tarde'
+          ),
+      });
   }
 
-  navigate(){
-    this.router.navigate(["login"])
+  navigate() {
+    this.router.navigate(['login']);
   }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -4,28 +4,56 @@ import { LoginResponse } from '../types/login-response.type';
 import { tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-  apiUrl: string = "https://mobibank.com.br/api"
+  apiUrl: string = 'https://mobibank.com.br/api';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  login(email: string, password: string){
-    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
-      tap((value) => {
-        sessionStorage.setItem("auth-token", value.token)
-        sessionStorage.setItem("username", value.name)
-      })
-    )
+  login(email: string, password: string) {
+    return this.httpClient
+      .post<LoginResponse>(this.apiUrl + '/login', { email, password })
+      .pipe(
+        tap((value) => {
+          sessionStorage.setItem('auth-token', value.token);
+          sessionStorage.setItem('username', value.name);
+        })
+      );
   }
 
-  signup(name: string, email: string, cep: string, cpfCnpj: string, birthDate: Date, phoneNumber: string, password: string){
-    return this.httpClient.post<LoginResponse>(this.apiUrl + "/signup", { name, email, password }).pipe(
-      tap((value) => {
-        sessionStorage.setItem("auth-token", value.token)
-        sessionStorage.setItem("username", value.name)
+  signup(
+    name: string,
+    email: string,
+    cep: string,
+    street: string,
+    city: string,
+    state: string,
+    country: string,
+    cpfCnpj: string,
+    birthDate: Date,
+    phoneNumber: string,
+    password: string
+  ) {
+    return this.httpClient
+      .post<LoginResponse>(this.apiUrl + '/signup', {
+        name,
+        email,
+        cep,
+        cpfCnpj,
+        birthDate,
+        phoneNumber,
+        password,
+        street,
+        city,
+        state,
+        country,
       })
-    )
+      .pipe(
+        tap((value) => {
+          sessionStorage.setItem('auth-token', value.token);
+          sessionStorage.setItem('username', value.name);
+        })
+      );
   }
 }
